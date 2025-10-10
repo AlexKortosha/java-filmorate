02c0460f172a3480ddb071fc6b03c065d55113a2
AlexKortosha <aaalexskortoshka@gmail.com>
2025-10-10 18:23:37 +0600

    Добавить аннотации в User

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,13 +1,24 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import java.time.LocalDate;
 
 @Data
 public class User {
     private Long id;
+
+    @NotBlank(message = "email должен быть указан")
+    @Email(message = "email должен содержать символ '@'")
     private String email;
+
+    @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "^\\S+$", message = "Логин не может содержать пробелы")
     private String login;
+
     private String name;
+
+    @NotNull(message = "Дата рождения должна быть указана")
+    @PastOrPresent(message = "Дата рождения не может быть в будущем")
     private LocalDate birthday;
 }
