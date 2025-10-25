d6c094707bd3bf747df88c512bd7c3b925e6d601
AlexKortosha <aaalexskortoshka@gmail.com>
2025-10-25 15:46:19 +0600

    add FilmService metods

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,4 +1,52 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class FilmService {
+    private final FilmStorage filmStorage;
+    private final UserStorage userStorage;
+
+    public void addLike(Long filmId, Long userId) {
+        Film film = filmStorage.getById(filmId);
+        User user = userStorage.getById(userId);
+
+        if (film == null || user == null) {
+            throw new ValidationException("Фильм или пользователь не найден");
+        }
+
+        film.getLikes().add(userId);
+        log.info("Пользователь {} поставил лайк фильму {}", userId, filmId);
+    }
+
+    public void removeLike(Long filmId, Long userId) {
+        Film film = filmStorage.getById(filmId);
+        User user = userStorage.getById(userId);
+
+        if (film == null || user == null) {
+            throw new ValidationException("Фильм или пользователь не найден");
+        }
+
+        film.getLikes().remove(userId);
+        log.info("Пользователь {} удалил лайк с фильма {}", userId, filmId);
+    }
+
+    public List<Film> getMostPopularFilms(int count) {
+        return filmStorage.findAll().stream()
+                .sorted(Comparator.comparingInt((Film f) -> f.getLikes().size()).reversed())
+                .limit(count)
+                .collect(Collectors.toList());
+    }
 }
