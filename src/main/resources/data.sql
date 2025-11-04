02c9963e294593d4cdc3d91b3f1e1c3ce0f378f6
AlexKortosha <aaalexskortoshka@gmail.com>
2025-11-05 03:32:07 +0600

    fix database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,12 @@
-INSERT INTO rating (name)
-VALUES ('G'),
-       ('PG'),
-       ('PG-13'),
-       ('R'),
-       ('NC-17');
+MERGE INTO rating (rating_id, name) VALUES (1, 'G');
+MERGE INTO rating (rating_id, name) VALUES (2, 'PG');
+MERGE INTO rating (rating_id, name) VALUES (3, 'PG-13');
+MERGE INTO rating (rating_id, name) VALUES (4, 'R');
+MERGE INTO rating (rating_id, name) VALUES (5, 'NC-17');
 
-INSERT INTO genre (name)
-VALUES ('Документальный'),
-       ('Комедия'),
-       ('Драма'),
-       ('Фантастика'),
-       ('Хоррор'),
-       ('Мелодрама'),
-       ('Мультфильм'),
-       ('Триллер');
+MERGE INTO genre (genre_id, name) VALUES (1, 'Комедия');
+MERGE INTO genre (genre_id, name) VALUES (2, 'Драма');
+MERGE INTO genre (genre_id, name) VALUES (3, 'Мультфильм');
+MERGE INTO genre (genre_id, name) VALUES (4, 'Триллер');
+MERGE INTO genre (genre_id, name) VALUES (5, 'Документальный');
+MERGE INTO genre (genre_id, name) VALUES (6, 'Боевик');
