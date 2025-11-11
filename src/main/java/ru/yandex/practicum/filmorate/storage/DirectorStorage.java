1db97d41d8ae2ac9afb0cde924ae40f1ad98ee44
Anton Riabushkin <riabushkinanton@yandex.ru>
2025-11-11 20:31:06 +0700

    создать DirectorDbStorage.

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/DirectorStorage.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Director;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface DirectorStorage {
+
+    Collection<Director> findAll();
+
+    Director add(Director director);
+
+    Director update(Director director);
+
+    Optional<Director> getById(Long id);
+
+    void removeDirector(Long id);
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/database/DirectorDbStorage.java
@@ -0,0 +1,72 @@
+package ru.yandex.practicum.filmorate.storage.database;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.mapper.DirectorRowMapper;
+import ru.yandex.practicum.filmorate.model.Director;
+import ru.yandex.practicum.filmorate.storage.DirectorStorage;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DirectorDbStorage implements DirectorStorage {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final DirectorRowMapper directorRowMapper = new DirectorRowMapper();
+
+    @Override
+    public List<Director> findAll() {
+        String sql = "SELECT * FROM director ORDER BY director_id";
+        return jdbcTemplate.query(sql, directorRowMapper);
+    }
+
+    @Override
+    public Director add(Director director) {
+        String sql = "INSERT INTO director (name) VALUES (?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, director.getName());
+            return preparedStatement;
+        }, keyHolder);
+
+        director.setId(Objects.requireNonNull(keyHolder.getKey()).longValue());
+        log.info("Добавлен директор {} с id={}", director.getName(), director.getId());
+        return director;
+    }
+
+    @Override
+    public Director update(Director director) {
+        String sql = "UPDATE director SET name = ? WHERE director_id = ?";
+
+        jdbcTemplate.update(sql,
+                director.getName(),
+                director.getId());
+
+        return director;
+    }
+
+    @Override
+    public Optional<Director> getById(Long id) {
+        String sql = "SELECT * FROM director WHERE director_id = ?";
+        List<Director> result = jdbcTemplate.query(sql, directorRowMapper, id);
+        return result.stream().findFirst();
+    }
+
+    @Override
+    public void removeDirector(Long id) {
+        String sql = "DELETE FROM director WHERE director_id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+}
