c85c58981a2cd90b09111f0f53cf704dd8b9a8ee
AlexKortosha <aaalexskortoshka@gmail.com>
2025-11-14 21:43:58 +0600

    add deleteFilm controller

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -36,6 +36,12 @@ public class FilmController {
         return ResponseEntity.ok(updated);
     }
 
+    @DeleteMapping
+    public ResponseEntity<Film> deleteFilm(@RequestBody Film film) {
+        Film deleted = filmService.deleteFilm(film);
+        return ResponseEntity.ok(deleted);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<Film> getFilm(@PathVariable Long id) {
         return ResponseEntity.ok(filmService.getFilm(id));
